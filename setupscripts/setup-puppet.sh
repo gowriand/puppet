24e7cf497f43fdd37e57655722a3e2a4e7be36ed
gowriand <95476026+gowriand@users.noreply.github.com>
2022-02-21 22:58:26 +0530

    Update setup-puppet.sh

--- setupscripts/setup-puppet.sh
+++ setupscripts/setup-puppet.sh
@@ -4,10 +4,10 @@ apt install -y net-tools
 name=master.ubuntu.com
 ip=`ifconfig |grep "broadcast 172." |grep -v 127.0.0.1 |awk '{print $2}'`
 echo "$name"  > /etc/hostname
-sed -i "1 s/^/$ip $name puppet puppetserver\n/" /etc/hosts
+sudo sed -i "1 s/^/$ip $name puppet \n/" /etc/hosts
 
 wget -O /tmp/puppet7-release-focal.deb https://apt.puppet.com/puppet7-release-focal.deb
 sudo dpkg -i /tmp/puppet7-release-focal.deb
 sudo apt-get update
 
-reboot
+sudo reboot
